57fd102153041db45ede766563e80b9583519ea7
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-10-29 17:38:19 +0530

    Update execute.sh

--- DevOps_Project_1/execute.sh
+++ DevOps_Project_1/execute.sh
@@ -1,16 +1,30 @@
-DOCKER_IMAGE="044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest"
+set -ex
 
+VERSION=$TAG
+cd ${WORKSPACE}/DevOps_Project_1
+
+export AWS_PROFILE="euran_devops_project"
+
+echo $AWS_PROFILE
+
+whoami
 aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin "044967670847.dkr.ecr.ap-south-1.amazonaws.com"
 
-docker build -t nginx-application .
+sudo docker build -t nginx-application .
 
-docker tag nginx-application:latest 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest
+sudo docker tag nginx-application:latest 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION
 
-docker push 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest
+sudo docker push 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION
 
-sed -i "s/devops_image/$DOCKER_IMAGE/g" nginx_deployment.yaml
+DOCKER_IMAGE="044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION"
+
+sed -i "s@devops_image@$DOCKER_IMAGE@g" nginx_deployment.yaml
+
+cat nginx_deployment.yaml
+
 
 aws eks update-kubeconfig --name mycluster --region ap-south-1 --profile euran_devops_project
 
 kubectl apply -f nginx_deployment.yaml
 
+kubectl expose deployment nginx-deployment --type=LoadBalancer --name=nginx-service
