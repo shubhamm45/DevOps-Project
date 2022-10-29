eef16261b13220e84c3bd3dbea29fc64694b582c
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-10-29 13:32:02 +0530

    Create execute.sh

---
+++ DevOps_Project_1/execute.sh
@@ -0,0 +1,14 @@
+DOCKER_IMAGE="044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest"
+
+aws ecr get-login-password --region ap-south-1 --profile euran_devops_project | docker login --username AWS --password-stdin 044967670847.dkr.ecr.ap-south-1.amazonaws.com
+
+docker build -t nginx-application .
+
+docker tag nginx-application:latest 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest
+
+docker push 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:latest
+
+sed -i "s/devops_image/$DOCKER_IMAGE/g" nginx_deployment.yaml
+
+kubectl apply -f nginx_deployment.yaml
+
