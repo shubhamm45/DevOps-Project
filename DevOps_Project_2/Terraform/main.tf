5835b8eafc935343c0927f791eed818e50f1d7a1
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-11-19 09:48:47 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,3 +1,17 @@
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "dev_machine" {
   ami = "ami-0e6329e222e662a52"
   instance_type = "t2.micro"
