7dd4fc4101730b4c7d10382855310f353382d850
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-11-20 12:58:12 +0530

    Create index.php

---
+++ DevOps_Project_2/Ansible/index.php
@@ -0,0 +1,7 @@
+<body bgcolor='white'>
+<pre>
+<?php
+print("<b><font color=\"blue\">!! Hello !! This is Euran,\n\n! Welcome ! to My Web Server</font>\n\n");
+print (`/usr/sbin/ifconfig`);
+?>
+</pre>
