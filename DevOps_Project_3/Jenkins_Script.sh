dee6929111c58315fe59f625eb120525bee877ac
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-12-18 14:16:46 +0530

    Create Jenkins_Script.sh

---
+++ DevOps_Project_3/Jenkins_Script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+export AWS_PROFILE="euran_devops_project"
+export REGION="ap-south-1"
+export TF_VAR_cluster_name=$CLUSTER_NAME
+
+#goint to the terraform path
+cd ${WORKSPACE}/DevOps_Project_3/Terraform
+
+#replacing the cluster name field in backend.tf file
+sed -i "s/ngg_cluster_name/my-cluster/g" backend.tf
+
+#running terraform command
+terraform init -migrate-state     
+terraform plan
+terraform $ACTION --auto-approve
+
+#login into the eks cluster
+aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION --profile $AWS_PROFILE
+
+#installing bitnami helm chart
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install nginx bitnami/nginx
+
+
+
