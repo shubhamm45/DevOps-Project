0429f98fcdd3e40c02b3753a6578c1bbffad2145
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-12-30 22:53:56 +0530

    Update backend.tf

--- DevOps_Project_3/Terraform/backend.tf
+++ DevOps_Project_3/Terraform/backend.tf
@@ -1,6 +1,9 @@
+
+
 terraform {
   backend "s3" {
-    key = "eks/cluster_name/statefile"
+    bucket = "euran-terraform-statefile"
+    key = "eks/ngg_cluster_name/statefile"
     region = "ap-south-1"
   }
 } 
