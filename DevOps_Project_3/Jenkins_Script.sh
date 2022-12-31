b3117bc655b23de75b11200d6f308fd04addc02d
shubhamm45 <60311835+shubhamm45@users.noreply.github.com>
2022-12-31 19:48:42 +0530

    Update Jenkins_Script.sh

--- DevOps_Project_3/Jenkins_Script.sh
+++ DevOps_Project_3/Jenkins_Script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-export AWS_PROFILE="euran_devops_project"
-export REGION="ap-south-1"
+
+export TF_VAR_region=$REGION
+export TF_VAR_vpc_id=$VPC_ID
 export TF_VAR_cluster_name=$CLUSTER_NAME
 
 #goint to the terraform path
@@ -12,16 +13,24 @@ cd ${WORKSPACE}/DevOps_Project_3/Terraform
 sed -i "s/cluster_name/my-cluster/g" backend.tf
 
 #running terraform command
-terraform init -migrate-state     
+terraform init    
 terraform plan
 terraform $ACTION --auto-approve
 
+if [ $ACTION == "apply" ]; then
 #login into the eks cluster
-aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION --profile $AWS_PROFILE
+aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION
+kubectl get pods -A
 
 #installing bitnami helm chart
 helm repo add bitnami https://charts.bitnami.com/bitnami
+
+#installing nginx
 helm install nginx bitnami/nginx
 
+#installing grafana
+helm install grafana bitnami/grafana
 
-
+else
+echo "no need to install"
+fi
